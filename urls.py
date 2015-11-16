19ccc8f0625d64243efed1a1756dfa22d6df6f5f
Alec Lownes <alec.lownes@gmail.com>
2015-11-16 12:44:50 -0500

    Add token authentication. Add view to get all checklists.

--- urls.py
+++ urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^api-token-auth/', authViews.obtain_auth_token),
+    url(r'^get-all-checklists/$', views.GetAllChecklistsView.as_view(), name='get-all-checklists'),
 ]
--- views.py
+++ views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+from.models import Checklist, ChecklistItem
 
 # Create your views here.
+class GetAllChecklistsView(APIView):
+    """
+    Authentication is needed for this methods
+    """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        checklists = []
+        for checklist in Checklist.objects.filter(owner=request.user.id):
+            checklistJson = {}
+            checklistItems = []
+            checklistJson['title'] = checklist.title
+            if checklist.parent is not None:
+                checklist = checklist.parent
+            for checklistItem in checklist.checklistitem_set.all().order_by('order'):
+                item = {}
+                item['text'] = checklistItem.text
+                item['checkable'] = checklistItem.checkable
+                item['checked'] = checklistItem.checked
+                checklistItems.append(item)
+            checklistJson['items'] = checklistItems
+            checklists.append(checklistJson)
+        return JsonResponse({'checklists':checklists})
