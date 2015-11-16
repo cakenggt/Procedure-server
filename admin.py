a473331dd0fb5411beaf962f8a587d7d419886da
Alec Lownes <alec.lownes@gmail.com>
2015-11-16 09:39:17 -0500

    Admin changes

--- admin.py
+++ admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Checklist, ChecklistItem, ChecklistEntry
+
+class ChecklistItemInline(admin.StackedInline):
+    model = ChecklistItem
+    extra = 3
+
+class ChecklistAdmin(admin.ModelAdmin):
+    inlines = [ChecklistItemInline]
+
 # Register your models here.
+admin.site.register(Checklist, ChecklistAdmin)
